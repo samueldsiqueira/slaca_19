c5f13e92f65db383f1fb6de0acfaf32292292876
samueldsiqueira <41975168+samueldsiqueira@users.noreply.github.com>
2022-05-07 21:18:25 -0300

    create app base components (top/side/footer)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SideNavbarComponent } from './components/side-navbar/side-navbar.component';
+import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SideNavbarComponent,
+    TopNavbarComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ]
